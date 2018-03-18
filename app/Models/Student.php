778ca17c6c901342c35e60a5f6282526d972e837
Claud Sumbus <claudiosumburane@gmail.com>
2018-03-18 21:12:11 +0200

    Add some models

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    protected $primaryKey='student_id';
+    protected $primaryKey='id';
 
     protected $table = 'students';
 
