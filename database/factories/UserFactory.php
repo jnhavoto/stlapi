103cbefdc79466c088420cc52740557470f9fe11
erciliomarques <erciliomarquesmanhica@gmail.com>
2018-03-18 22:16:09 +0200

    data base and api are now linked

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,12 +12,36 @@ use Faker\Generator as Faker;
 | model instances for testing / seeding your application's database.
 |
 */
+//
+//$factory->define(App\User::class, function (Faker $faker) {
+//    return [
+//        'name' => $faker->name,
+//        'email' => $faker->unique()->safeEmail,
+//        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+//        'remember_token' => str_random(10),
+//    ];
+//});
 
-$factory->define(App\User::class, function (Faker $faker) {
+$cursos = ['Computer Science','Network Admin','Database designer']; //array de dados
+
+$factory->define(\App\Models\Course::class, function (Faker $faker) use ($cursos) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+
+        'name' => $faker->unique()->randomElement($cursos),
+        'course_content' => $faker->text(10),
+        'departments_id' => $faker->numberBetween(1, \App\models\Department::all()->count()),
+
+
     ];
 });
+
+$department_names = ['Science','Health']; //array de dados
+$factory->define(\App\Models\Department::class, function (Faker $faker) use ($department_names) {
+    return [
+
+        'name' => $faker->unique()->randomElement($department_names),
+
+
+    ];
+});
+
--- database/migrations/2018_03_18_201310_create_departments_table.php
+++ database/migrations/2018_03_18_201310_create_departments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateCoursesTable extends Migration {
+class CreateDepartmentsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,12 +12,13 @@ class CreateCoursesTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('courses', function(Blueprint $table)
+		Schema::create('departments', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
-			$table->text('course_content', 65535);
-			$table->integer('departments_id')->unsigned()->index('fk_courses_departments1_idx');
+			$table->text('name', 65535);
+			$table->dateTime('created_at')->nullable();
+			$table->dateTime('update_at')->nullable();
+			$table->softDeletes();
 		});
 	}
 
@@ -29,7 +30,7 @@ class CreateCoursesTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('courses');
+		Schema::drop('departments');
 	}
 
 }
