b154c117546e60da6f4088e8a67992854b0e183d
Claud Sumbus <claudiosumburane@gmail.com>
2018-03-18 21:24:18 +0200

    Add some models again

--- app/Models/AssignmentDescription.php
+++ app/Models/AssignmentDescription.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class AssignmentDescription extends Model
 {
-    //
+    protected $primaryKey='id';
+
+    protected $table = 'assignment_descriptions';
+
+    protected $fillable = [
+        'case', 'instructions', 'startdate', 'deadline', '	available_date', 'teacher_courses_id'
+    ];
 }
--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
-    //
+    protected $primaryKey='id';
+
+    protected $table = 'feedbacks';
+
+    protected $fillable = [
+        'goal', 'message', 'advice', 'comment', 'feedback_date', 'assignment_submissions_id', 'students_id'
+    ];
 }
