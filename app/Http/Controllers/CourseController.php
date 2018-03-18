c0122e447beda70e949d16ee92bffbf95f6cc40a
erciliomarques <erciliomarquesmanhica@gmail.com>
2018-03-18 22:20:49 +0200

    starting the consume the api

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
@@ -13,6 +14,9 @@ class CourseController extends Controller
     public function getAllCourses()
     {
 
+        $courses= Course::all();
+
+        return response()->json(['courses'=>$courses],200);
     }
 
     /**
