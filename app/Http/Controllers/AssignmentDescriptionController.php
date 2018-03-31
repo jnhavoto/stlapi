3ac87b611aa7437d7c4efece27a0fbba9414975c
Herquiloide Raimundo Hele <herquiloide@gmail.com>
2018-03-31 17:05:43 +0200

    primeiro teste com assigment Descriptions

--- app/Http/Controllers/AssignmentDescriptionController.php
+++ app/Http/Controllers/AssignmentDescriptionController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AssignmentDescription;
 use Illuminate\Http\Request;
 
 class AssignmentDescriptionController extends ModelController
 {
 
+    public function __construct() {
+        $this->objecto = new AssignmentDescription();
+        $this->nomeObjecto = 'assignment_description';
+        $this->nomeObjectos = 'assignment_description';
+        $this->relacionados = [];
+    }
 
 
 }
