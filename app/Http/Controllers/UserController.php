d1dd4b3effc20f1673f97969ab2b93aba9ee0aed
Herquiloide Raimundo Hele <herquiloide@gmail.com>
2018-04-01 12:29:35 +0200

    configuracao inicaial da Autenticacao

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,5 +16,36 @@ class UserController extends Controller
     }
 
 
+    function login(Request $request){
+
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $credencias = $request->only(['email', 'password']);
+
+        try{
+            if(! $token = \Tymon\JWTAuth\JWTAuth::attempt($credencias))
+                return response()->json(['mensagem' => 'Credencias Erradas', 'status' => 401], 401);
+        }catch (JWTException $ex){
+            return response()->json(['mensagem' => 'Erro ao gerar token', 'status' => 500], 500);
+        }
+
+        $user = $this->getUser(new Request(['token' => $token]));
+
+        return response()->json(['token' => $token, 'user' => $user, 'status' => 200], 200);
+    }
+
+    function signup(){
+
+    }
+
+    function logout(){
+
+    }
+
+
+
 
 }
