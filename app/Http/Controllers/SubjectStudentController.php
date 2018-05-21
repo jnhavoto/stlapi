9974547cf5840c83444af6a83e5b4ba7d0b860d3
jnhavoto <janhavoto@gmail.com>
2018-05-21 21:58:45 +0200

    metodo para acttuallizar Disciplinas do Estudante

--- app/Http/Controllers/SubjectStudentController.php
+++ app/Http/Controllers/SubjectStudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use App\Models\SubjectsHasStudent;
 use Illuminate\Http\Request;
 
@@ -14,5 +15,42 @@ class SubjectStudentController extends ModelController
         $this->objectNames = 'subject_students';
         $this->relactionships = [];
     }
+
+
+
+    public function store(Request $request)
+    {
+       $studentSubjects = SubjectsHasStudent::where('student_id', '=', $request->get('student_id'))->get();
+
+
+        foreach ($studentSubjects as $studentSubject){
+           $contains = false;
+            foreach ($request->get('subject') as $subject){
+               if($studentSubjects['subjects_id'] == $subject['subjects_id'])
+                   $contains = true;
+           }
+
+           if(!$contains)
+               $studentSubject->delete();
+
+       }
+
+        foreach ($request->get('subject') as $subject){
+            $contains = false;
+            foreach ($studentSubjects as $studentSubject){
+                if($studentSubjects['subjects_id'] == $subject['subjects_id'])
+                    $contains = true;
+            }
+
+            if(!$contains)
+                SubjectsHasStudent::create($subject);
+
+        }
+
+
+        return ['subjects' => $studentSubjects = SubjectsHasStudent::where('student_id', '=', $request->get('student_id'))->get()];
+
+    }
+
 
 }
