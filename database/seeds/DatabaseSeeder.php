06cc1f957aab7e28ab2ae166f6097deb899dbede
jnhavoto <janhavoto@gmail.com>
2018-11-21 10:45:13 +0200

    small fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         factory(\App\Models\Course::class, 3)->create();
         factory(\App\Models\City::class, 5)->create();
         factory(\App\Models\School::class, 5)->create();
-        factory(\App\User::class, 2)->create();
+        factory(\App\User::class, 6)->create();
 //        factory(\App\Models\Teacher::class, 1)->create();
 //        factory(\App\Models\Student::class, 1)->create();
 //        factory(\App\Models\TeacherCourse::class, 5)->create();
